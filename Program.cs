44c678a22a7c6480123427672384a881cb51cd0a
Simeon Kanyov <86410900+SanchoPansa69@users.noreply.github.com>
2022-06-22 18:46:25 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace Zadacha
+namespace VSP_46455r_5
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Console.WriteLine("Domashno");
-            //Брой тухли = x
-            //Брой работници = w
-            //Вместимост на количката = m
-            //Напишете програма, която прочита целите числа x, w и m и пресмята колко най-малко курса трябва да направят работниците за да превозят тухлите.
-
-
-
-            Console.WriteLine("Broi tuhli");
-            double x = int.Parse(Console.ReadLine());
-            if (x > 1000 || x < 1)
-            { Console.WriteLine("Допустими са само числа от 1 до 1000"); return; }
-            Console.WriteLine("Broi rabotnitsi");
-            int w = int.Parse(Console.ReadLine());
-            if (w > 1000 || w <1)
-            { Console.WriteLine("Допустими са само числа от 1 до 1000"); return; }
-            Console.WriteLine("Kapatsitet na kolichkite");
-            int m = int.Parse(Console.ReadLine());
-            if (m > 1000 || m < 1)
-            { Console.WriteLine("Допустими са само числа от 1 до 1000"); return; }
-            Console.WriteLine("Minimalen broi kursove");
-            double i = x / (m * w);
-            Console.WriteLine(Math.Ceiling(i));
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
